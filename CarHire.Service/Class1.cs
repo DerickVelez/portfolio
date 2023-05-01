a85a1f2c53691adc3feac200caa3a4cd90b12924
Jan Derick Velez <jderickvelez@gmail.com>
2023-05-01 20:37:37 +0800

    Draft class for refresher

---
+++ CarHire.Service/Class1.cs
@@ -0,0 +1,31 @@
+﻿using CarHire_.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarHire.Service
+{
+    public class Class1
+    {
+        public List<Booking> bookinglist = new List<Booking> { };
+
+        public List<Booking> GetBookings()
+        {
+            return bookinglist;
+        }
+
+        public void CreateBooking(Booking booking)
+        {
+            bookinglist.Add(booking);
+        }
+
+        public List<Booking> DeleteBooking(Booking booking)
+        {
+            //var finalBooking = bookinglist.Where(a => a.BookingID == booking.BookingID).FirstOrDefault();
+            //return bookinglist.Remove(finalBooking);
+        }
+    }
+}
+
