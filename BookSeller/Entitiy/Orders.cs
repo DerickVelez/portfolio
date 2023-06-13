43eb1822a4faa0adfedd6cf5ad8b07dbe198326b
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-13 08:06:20 +0800

    Added Order property on Entities folder

---
+++ BookSeller/Entitiy/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSeller.Entitiy
+{
+    public class Orders
+    {
+        public DateTime OrderDate  { get; set; }
+
+        public string OrderValue { get; set; }
+    }
+}
