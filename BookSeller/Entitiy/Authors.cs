f84542ecbaad058f62fa526dbe6bd0644f687509
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-14 11:05:10 +0800

    Added Author property in Entities folder

---
+++ BookSeller/Entitiy/Authors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSeller.Entitiy
+{
+    public class Authors
+    {
+        public int AuthorId { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorInitials { get; set; }
+        public string AuthorLastName { get; set; }
+    }
+}
