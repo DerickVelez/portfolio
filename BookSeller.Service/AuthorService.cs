3a5f02dc72b389c4ecac35163ae23122d1a0ec4f
Jan Derick Velez <jderickvelez@gmail.com>
2023-06-23 22:12:05 +0800

    Added Author Service

---
+++ BookSeller.Service/AuthorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSeller.Service
+{
+    public class AuthorService
+    {
+        public List<string> GetAuthorList()
+        {
+            return new List<string>();
+        }
+    }
+}
