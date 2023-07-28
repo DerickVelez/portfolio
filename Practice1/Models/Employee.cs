3d9f3dc2f265a312668bbd9eb07d8b7e1acd8ff8
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-28 21:35:36 +0800

    Added Project for Practice1

---
+++ Practice1/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practice1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+    }
+}
