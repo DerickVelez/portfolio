350efbbc1ff20fa1c957bc52125650c53fb73686
Jan Derick Velez <jderickvelez@gmail.com>
2023-07-30 20:12:29 +0800

    Added Employee Controller

---
+++ Practice1/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Practice1.Models;
+
+namespace Practice1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeController(Practice1Context dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public Practice1Context DbContext { get; }
+
+        [HttpGet]
+        public List<Employee> GetEmployees()
+        {
+            return DbContext.Employees.ToList();
+        }
+
+        [HttpPost]
+        public Employee CreateEmployees(Employee employee)
+        {
+            var createdEmployee = DbContext.Employees.Add(employee);
+            DbContext.SaveChanges();
+            return createdEmployee.Entity;
+        }
+
+        [HttpPatch]
+        public Employee UpdateEmployees(int employeeId,Employee employee) 
+        {
+            var existingEmployee = DbContext.Employees.Find(employeeId);
+
+            existingEmployee.Name = employee.Name;
+
+            DbContext.SaveChanges();
+
+            return new Employee
+            {
+                Name = employee.Name,
+            };
+        }
+
+        [HttpDelete]
+        public Employee DeleteEmployee(Employee employee)
+        {
+           DbContext.Employees.Remove(employee);
+            DbContext.SaveChanges();
+            return employee;
+        }
+    }
+}
+
+
+       
